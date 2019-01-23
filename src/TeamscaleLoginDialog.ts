24196a618bed9f02e24f80d6f8e5ba781e4d7b7f
Thomas Pettinger <pettinger@cqse.eu>
2019-01-23 17:22:01 +0100

    Auto-detect login in Teamscale dialog

--- src/TeamscaleLoginDialog.ts
+++ src/TeamscaleLoginDialog.ts
@@ -16,8 +16,17 @@ VSS.ready(() => {
     projectSettings.get(Settings.TEAMSCALE_URL).then(url => {
         const body = $('body,html');
         console.log(`width: ${body.width()}, height: ${body.height()}`);
-        body.html(`<iframe width="520" height="634" src="${url}"></iframe>`)
+        body.html(`<iframe id="teamscale-frame" width="520" height="634" src="${url}"></iframe>`);
+        document.getElementById('teamscale-frame').onload = teamscaleLoginLoadedCallback;
     });
 
     VSS.notifyLoadSucceeded();
 });
+
+function teamscaleLoginLoadedCallback() {
+    document.getElementById('teamscale-frame').onload = teamscaleLoggedInCallback;
+}
+
+function teamscaleLoggedInCallback() {
+    console.log("Logged in");
+}

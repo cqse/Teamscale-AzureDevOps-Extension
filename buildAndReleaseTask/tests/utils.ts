f8be7baab7e890d7afd0ec8f1d30c474b8bab45b
Fabian Streitel <streitel@cqse.eu>
2019-04-02 13:19:50 +0200

    add more tests

--- buildAndReleaseTask/tests/utils.ts
+++ buildAndReleaseTask/tests/utils.ts
@@ -1,5 +1,6 @@
 import * as utils from '../utils';
 import * as assert from 'assert';
+import * as path from 'path';
 
 describe('firstWildcardIndex', function() {
     it('should return null if no wildcards present', function() {
@@ -15,6 +16,7 @@ describe('firstWildcardIndex', function() {
     });
 });
 
+const fixturePath = path.join(__dirname, "fixtures");
 
 describe('resolveFiles', function() {
     it('should throw an exception if an absolutely specified file does not exist', function() {
@@ -23,5 +25,14 @@ describe('resolveFiles', function() {
     
     it('should throw an exception if pattern matches no files', function() {
         assert.throws(() => utils.resolveFiles("does/not/exist*.xml"), /Did not find/);
+    });
+
+    it('should return a single file when it exists', function() {
+        assert.deepEqual(utils.resolveFiles(`${fixturePath}/coverage.simple`), [`${fixturePath}/coverage.simple`]);
+    });
+
+    it('should return all matching files', function() {
+        assert.deepEqual(utils.resolveFiles(`${fixturePath}/**.simple`), 
+                [`${fixturePath}/coverage.simple`, `${fixturePath}/coverage2.simple`]);
     });
 });

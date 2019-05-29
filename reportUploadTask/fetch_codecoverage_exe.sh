b6d20be44c6a9e30474f7354e6d8c39a03cd82b7
Fabian Streitel <streitel@cqse.eu>
2019-05-29 08:10:11 +0200

    fix cp for MacOS

--- reportUploadTask/fetch_codecoverage_exe.sh
+++ reportUploadTask/fetch_codecoverage_exe.sh
@@ -4,6 +4,6 @@ mkdir tmp
 cd tmp
 curl https://www.nuget.org/api/v2/package/Microsoft.CodeCoverage/16.0.1 -L -o microsoft.coverage.nupkg
 unzip microsoft.coverage.nupkg
-cp build/netstandard1.0/CodeCoverage -r ..
+cp -r build/netstandard1.0/CodeCoverage ..
 cd ..
 rm -r tmp

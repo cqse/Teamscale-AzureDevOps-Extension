2ee18ece5b13a554e445c8d55aff7d71b272baa6
Thomas Pettinger <pettinger@cqse.eu>
2024-11-14 15:38:53 +0100

    TS-0 Also rename plugin upload tasks and several contribution titles within the plugin

--- prepare-for-tests.sh
+++ prepare-for-tests.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 sed -i 's/"id": "teamscale-azure-devops-plugin",/"id": "teamscale-azure-devops-plugin-test3",/' vss-extension.json
 sed -i 's/"public": true,/"public": false,/' vss-extension.json
+sed -i 's/"name": "Teamscale DevOps",/"name": "Teamscale DevOps Test",/' vss-extension.json
+sed -i 's/"name": "Teamscale",/"name": "Teamscale Test",/g' vss-extension.json
 sed -i 's/"id": "FA6BEEDE-4FE1-11E9-8D71-071835DA2614",/"id": "8e8b64dc-cc55-4bd1-85da-7b5fde8efea3",/' reportUploadTask/task.json
+sed -i 's/"name": "TeamscaleReportUploader",/"name": "TeamscaleReportUploaderTest",/' reportUploadTask/task.json
+sed -i 's/"friendlyName": "Teamscale Report Uploader",/"friendlyName": "Teamscale Report Uploader Test ",/' reportUploadTask/task.json
